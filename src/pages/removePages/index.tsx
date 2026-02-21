5bd7889b83f3789934f3b63f341f597942c8ecf2
Prashant Sachan <your-email@example.com>
2026-02-21 23:27:55 +0530

    feat: Update sitemap change frequency to monthly and explicitly pass canvas to PDF rendering options.

--- src/pages/removePages/index.tsx
+++ src/pages/removePages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import Header from "../../component/header";
 import SEO from "../../component/seo";
 import StructuredData from "../../component/seo/StructuredData";
@@ -83,6 +83,7 @@ const RemovePages = () => {
           canvas.width = viewport.width;
 
           await page.render({
+            canvas: canvas,
             canvasContext: context,
             viewport: viewport,
           }).promise;
